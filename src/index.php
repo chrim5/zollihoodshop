ff1c394794bc313871d1b7bb3c3305e1d1e0553d
Marco Schmid <marco@marcoschmid.me>
2016-09-26 19:38:31 +0200

    moved html-content

--- src/index.php
+++ src/index.php
@@ -1,33 +1,5 @@
-<html>
-<head>
-<title>test</title>
-</head>
-<body>
 <?php
 require 'db.php';
+readfile('main.html');
 ?>
 
-
-<div class="container">
-    <header>
-        <h1>Amazing Outstanding Computershop</h1>
-    </header>
-
-    <nav>
-      <ul>
-        <li><a href="#">Bla 1</a></li>
-        <li><a href="#">Bla 2</a></li>
-        <li><a href="#">Bla 3</a></li>
-      </ul>
-    </nav>
-
-    <article>
-      <h1>Artikel 1</h1>
-      <p>Blaaa blaaaa blaaaaa </p>
-    </article>
-
-    <footer>Copyright © nobody</footer>
-
-</div>
-</body>
-</html>
