faea2d2789ceb57e920ec3116b6a674edcdc4ab6
Marco Schmid <marco@marcoschmid.me>
2016-11-04 14:52:33 +0100

    removed browsersync

--- src/footer.php
+++ src/footer.php
@@ -232,14 +232,6 @@
 
 	</div>
 	<!-- End Shell -->
-
-	<!-- Browser Sync plugin -->
-	<script id="__bs_script__">//<![CDATA[
-  document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.17.0'><\/script>".replace("HOST", location.hostname));
-	//]]>
-	</script>
-	<!-- End Browser Sync plugin -->
-
 </body>
 
 </html>
