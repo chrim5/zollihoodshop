71e0a684bef1b288e7cedf152614d43d9b991e19
Marco Schmid <marco@marcoschmid.me>
2016-11-04 16:05:45 +0100

    added singleton for db

--- src/db.php
+++ src/db.php
@@ -1,14 +1,33 @@
 <?php
-$servername = $_ENV["MYSQL_PORT_3306_TCP_ADDR"];
-$username = "root";
-$password = $_ENV["MYSQL_ENV_MYSQL_ROOT_PASSWORD"];
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+
+class DB {
+    static private $_instance;
+    private $_connection;
+
+
+    private function __construct() {
+        $servername = $_ENV["MYSQL_PORT_3306_TCP_ADDR"];
+        $username = "root";
+        $password = $_ENV["MYSQL_ENV_MYSQL_ROOT_PASSWORD"];
+
+        $this->_connection = new mysqli($servername, $username, $password, "myapp");
+        if ($this->_connection->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+    }
+    
+    public static function getInstance(){
+        if (!self::$_instance){
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    } 
+
+    public function getConnection(){
+        return $this->_connection;
+    }
+}
 ?>
 
--- src/support.php
+++ src/support.php
@@ -2,5 +2,11 @@
 <?php require_once ('ui/header.php') ?>
 
 support
+<?php 
+    require_once("db.php");
+    $db = DB::getInstance();
+    $db->getConnection();
+ ?>
+
 
 <?php require_once ('ui/footer.php') ?>
