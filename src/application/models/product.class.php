208fbfeecea73be730893af58c4a23f229c28c67
Marco Schmid <marco@marcoschmid.me>
2016-12-05 17:18:37 +0100

    simple table for product-display

--- src/application/models/product.class.php
+++ src/application/models/product.class.php
@@ -2,7 +2,7 @@
 
 class ProductObj
 {
-    private $id, $name, $details, $price, $reducedprice, $category;
+    public $id, $name, $details, $price, $reducedprice, $category;
     
     public function __toString() {
         return $this->name;
--- src/application/views/product/index.php
+++ src/application/views/product/index.php
@@ -1,4 +1,25 @@
 <div class="products">
-    <?php var_dump($products); ?>
+<table>
+  <thead align="left" style="display: table-header-group">
+  <tr>
+     <th>Num </th>
+     <th>Name</th>
+     <th>Price</th>
+     <th>Category</th>
+  </tr>
+  </thead>
+<tbody>
+<?php 
+$total = 0;
+foreach ($products as $rows) :?>
+  <tr class="item_row">
+        <td> <?php echo $rows->id; ?></td>
+        <td> <?php echo $rows->name; ?></td>
+        <td> <?php echo $rows->price; ?></td>
+        <td> <?php echo $rows->category; ?></td>
+  </tr>
+</tbody>
+</table>
+<?php endforeach;?>
 </div>
 
