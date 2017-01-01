00e6eb35629001ed31e6c1fe744b91c86053ae3c
Marco Schmid <marco@marcoschmid.me>
2017-01-01 22:04:29 +0100

    changed list layout

--- src/application/views/product/index.php
+++ src/application/views/product/index.php
@@ -7,16 +7,16 @@ foreach ($products as $rows) :?>
         <div class="itemwrapper">
             <div class="leftiteminfo">
                 <img src="/product/image/<?php echo $rows->id; ?>" width="130">
+                <a href="/product/addimage/<?php echo $rows->id; ?>">change image</a>
             </div>
             <div class="rightiteminfo">
-<h2>
-                <?php echo $rows->name; ?>
-</h2>
-                <?php echo $rows->price; ?>
+                <h2><?php echo $rows->name; ?></h2>
+                <h3><?php echo $rows->price; ?>.- CHF</h3>
                 <a href="cart/add/<?php echo $rows->id; ?>">add to cart</a>
             </div>
         </div>
     </li>
 <?php endforeach;?>
 </ul>
+<a href="/product/additem">new item</a>
 </div>
