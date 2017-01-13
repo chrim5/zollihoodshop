e4abd1fb2cb933084dc73846f1313e374bf8dbe4
Marco Schmid <marco@marcoschmid.me>
2017-01-13 09:53:42 +0100

    fixed add to cart link

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,4 @@
-$.get( "cart/cartjson", function( data ) {
+$.get( "/cart/cartjson", function( data ) {
     $( "#cartprice" ).html( data.price + ".- CHF" );
     $( "#cartproducts" ).html( data.products );
 });
