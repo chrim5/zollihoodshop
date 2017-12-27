7835f6ef905e40cb6419ae16eec6096c7fe6244b
chrim5 <marc.christen@students.bfh.ch>
2017-12-27 22:46:28 +0100

    Header update

--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -5,7 +5,7 @@
 <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <meta content="initial-scale=1.0" name="viewport"/>
-    <title>MMs Book Store</title>
+    <title>Zollihoodshop</title>
 	<link rel="stylesheet" href="/css/style.css" type="text/css" media="all" />
     <link rel="icon" type="image/ico" href="/css/images/favicon.ico">
 
